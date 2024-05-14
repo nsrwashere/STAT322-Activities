c9e06d9eaa3869f3f2647f1745527e4c219c03a3
nsrwashere <169031186+nsrwashere@users.noreply.github.com>
2024-05-14 08:55:28 +0800

    Update and rename NRoque_Activity 5 (1 of 3)_STAT322.py to NRoque_Activity 5_STAT322.py
    Coding of sample script.

--- NRoque_Activity 5_STAT322.py
+++ NRoque_Activity 5_STAT322.py
@@ -4,8 +4,9 @@ Created on Tue May 14 08:16:29 2024
 
 @author: NRoque
 """
-# BAYESIAN STATISTICS IN PYTHON ---THE PRIOR
+# BAYESIAN STATISTICS IN PYTHON 
 
+#THE PRIOR
 import scipy.stats as sts 
 import numpy as np 
 import matplotlib.pyplot as plt 
@@ -21,3 +22,23 @@ plt.plot(mu, uniform_dist, label = 'Uniform Dist')
 plt.xlabel("Value of $\mu$ in meters") 
 plt.ylabel("Probability Density") 
 plt.legend()
+
+#THE LIKELIHOOD ---CAN BE PLOTTED IN DIFFERENT FILE TO NOT OVERLAP W/ PRIOR PLOT
+def likelihood_func(datum, mu): 
+    likelihood_out = sts.norm.pdf(datum, mu, scale = 0.1) 
+    return likelihood_out/likelihood_out.sum() 
+likelihood_out = likelihood_func(1.7, mu) 
+plt.plot(mu, likelihood_out) 
+plt.title("Likelihood of $\mu$ given observation 1.7m") 
+plt.ylabel("Probability Density/Likelihood") 
+plt.xlabel("Value of $\mu$") 
+plt.show()
+
+#THE POSTERIOR ---CANNOT BE PLOTTED IN DIFFERENT FILE
+import scipy as sp 
+
+unnormalized_posterior = likelihood_out * uniform_dist 
+plt.plot(mu, unnormalized_posterior) 
+plt.xlabel("$\mu$ in meters") 
+plt.ylabel("Unnormalized Posterior") 
+plt.show()
